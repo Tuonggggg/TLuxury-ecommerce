769328444b47211be08c486f5224191d69da17aa
Tuonggggg <tuantuong15052004@gmail.com>
2025-11-12 22:41:00 +0700

    update package.json

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 // src/lib/axios.js
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE == "development" ? "http://localhost:5000/api" : "/api";
+const BASE_URL = 
+  import.meta.env.MODE == "development" ? "http://localhost:5000/api" : "/api";
 
 // 💡 LƯU Ý: Tạo một instance KHÁC cho yêu cầu refresh để tránh vòng lặp interceptor
 const refreshInstance = axios.create({
